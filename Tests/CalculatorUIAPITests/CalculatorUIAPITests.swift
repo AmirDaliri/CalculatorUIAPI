8cfa02bebb71205483499864bf7eb09cb383c2d8
emir daliri <amir.dalirii69@gmail.com>
2021-07-20 16:16:25 +0300

    unit testing

--- Tests/CalculatorUIAPITests/CalculatorUIAPITests.swift
+++ Tests/CalculatorUIAPITests/CalculatorUIAPITests.swift
@@ -2,10 +2,59 @@
     @testable import CalculatorUIAPI
 
     final class CalculatorUIAPITests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(CalculatorUIAPI().add, true)
+        
+        func testDefaultCalculatorUIAPI() {
+            let featureToggle = CalculatorUIAPI(dictionary: nil)
+            XCTAssertTrue(featureToggle.add)
+            XCTAssertTrue(featureToggle.minus)
+            XCTAssertTrue(featureToggle.multiply)
+            XCTAssertTrue(featureToggle.divide)
+            XCTAssertTrue(featureToggle.cos)
+            XCTAssertTrue(featureToggle.sin)
+            XCTAssertTrue(featureToggle.bitcoinRate)
+        }
+        
+        func testCompletedCalculatorUIAPI() {
+            let dictionary = ["add" : false,
+                              "minus" : true,
+                              "multiply" : true,
+                              "divide" : true,
+                              "sin" : false,
+                              "cos" : true,
+                              "bitcoinRate" : true]
+            let featureToggle = CalculatorUIAPI(dictionary: dictionary)
+            XCTAssertFalse(featureToggle.add)
+            XCTAssertTrue(featureToggle.minus)
+            XCTAssertTrue(featureToggle.multiply)
+            XCTAssertTrue(featureToggle.divide)
+            XCTAssertTrue(featureToggle.cos)
+            XCTAssertFalse(featureToggle.sin)
+            XCTAssertTrue(featureToggle.bitcoinRate)
+        }
+        
+        func testCalculatorUIAPIWithDefaultValues() {
+            let dictionary = ["add" : false,
+                              "minus" : true,
+                              "divide" : true,
+                              "sin" : false]
+            let featureToggle = CalculatorUIAPI(dictionary: dictionary)
+            XCTAssertFalse(featureToggle.add)
+            XCTAssertTrue(featureToggle.minus)
+            XCTAssertTrue(featureToggle.multiply)
+            XCTAssertTrue(featureToggle.divide)
+            XCTAssertTrue(featureToggle.cos)
+            XCTAssertFalse(featureToggle.sin)
+            XCTAssertTrue(featureToggle.bitcoinRate)
+        }
+
+        func testOfflineCalculatorUIAPI() {
+            let featureToggle = CalculatorUIAPI.offline()
+            XCTAssertTrue(featureToggle.add)
+            XCTAssertTrue(featureToggle.minus)
+            XCTAssertTrue(featureToggle.multiply)
+            XCTAssertTrue(featureToggle.divide)
+            XCTAssertTrue(featureToggle.cos)
+            XCTAssertTrue(featureToggle.sin)
+            XCTAssertFalse(featureToggle.bitcoinRate)
         }
     }
